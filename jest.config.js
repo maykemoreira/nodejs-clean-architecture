8b2f22d65c496d56d7fa116bd2b34c7b663bf084
Mayke Moreira <mayke.moreira3@gmail.com>
2021-11-13 16:26:32 -0300

    feat: ensure mongodb reconnect if client is down

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/main/**',
     '!**/protocols/**'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
