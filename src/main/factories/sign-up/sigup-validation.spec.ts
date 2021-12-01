02e5b041510c2eea364728865f680c5f76a0b038
Mayke Moreira <mayke.moreira3@gmail.com>
2021-11-30 21:20:37 -0300

    feat: add LoginValidationFactory

--- src/main/factories/sign-up/sigup-validation.spec.ts
+++ src/main/factories/sign-up/sigup-validation.spec.ts
@@ -1,11 +1,11 @@
-import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
-import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
+import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
 import { makeSignupValidation } from './signup-validation'
-import { Validation } from '../../presentation/helpers/validators/validation'
-import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
-import { EmailValidation } from '../../presentation/helpers/validators/email-validation'
-import { EmailValidator } from '../../presentation/protocols/email-validator'
-jest.mock('../../presentation/helpers/validators/validation-composite')
+import { Validation } from '../../../presentation/helpers/validators/validation'
+import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation'
+import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
+import { EmailValidator } from '../../../presentation/protocols/email-validator'
+jest.mock('../../../presentation/helpers/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidationStub implements EmailValidator {
