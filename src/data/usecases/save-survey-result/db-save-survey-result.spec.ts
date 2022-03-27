f6c898c997bc05781c32e1512f3176023c673765
Mayke Moreira <mayke.moreira3@gmail.com>
2022-03-27 20:26:26 -0300

    test: ensure DbSaveSurveyResult returns a SurveyResult on success

--- src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -63,4 +63,10 @@ describe('DbSaveSurveyResult UseCase', () => {
     const promise = sut.save(makeFakeSurveyResultData())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return SurveyResult on success', async () => {
+    const { sut } = makeSut()
+    const surveyResultData = await sut.save(makeFakeSurveyResultData())
+    expect(surveyResultData).toEqual(makeFakeSurveyResult())
+  })
 })
