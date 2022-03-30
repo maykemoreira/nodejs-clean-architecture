7b93dfda65d7c3f8bde2fac0ad79233243a9eb48
Mayke Moreira <mayke.moreira3@gmail.com>
2022-03-29 23:07:26 -0300

    refactor: organize project files

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -2,7 +2,7 @@ import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import { SurveyModel } from '@/domain/models/survey'
 import { AddSurveyModel } from '@/domain/usecases/add-survey'
 import { AddSurveyRepository } from '@/data/protocols/db/survey/add-survey-repository'
-import { LoadSurveyByIdRepository } from '@/data/usecases/load-survey-by-id/db-load-survey-by-id-protocols'
+import { LoadSurveyByIdRepository } from '@/data/usecases/survey/load-survey-by-id/db-load-survey-by-id-protocols'
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveyByIdRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
--- src/main/factories/usecases/account/add-account/db-add-account-factory.ts
+++ src/main/factories/usecases/account/add-account/db-add-account-factory.ts
@@ -2,7 +2,7 @@
 import { AddAccount } from '@/domain/usecases/add-account'
 import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
 import { BcryptAdapter } from '@/infra/criptography/bcrypt-adapter/bcrypt-adapter'
-import { DbAddAccount } from '@/data/usecases/add-account/db-add-account'
+import { DbAddAccount } from '@/data/usecases/account/add-account/db-add-account'
 
 export const makeDbAddAccount = (): AddAccount => {
   const salt = 12
