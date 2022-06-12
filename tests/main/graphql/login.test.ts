dd01c304ad8e54d98c9f098cbf6c1eab11af0bae
Mayke Moreira <mayke.moreira3@gmail.com>
2022-06-12 18:36:26 -0300

    test: ensure  Login Query returns an account on valid credentials

---
+++ tests/main/graphql/login.test.ts
@@ -0,0 +1,55 @@
+import { MongoHelper } from '@/infra/db/mongodb/mongo-helper'
+import { makeApolloServer } from './helpers'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+import { createTestClient } from 'apollo-server-integration-testing'
+import { ApolloServer, gql } from 'apollo-server-express'
+
+let accountCollection: Collection
+let apolloServer: ApolloServer
+
+describe('Login GraphQL', () => {
+  beforeAll(async () => {
+    apolloServer = makeApolloServer()
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('Login Query', () => {
+    const loginQuery = gql`
+        query login($email: String!, $password: String!){
+            login(email: $email, password: $password) {
+                accessToken
+                name
+            }
+        }
+    `
+
+    test('Should return an account on valid credentials', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password
+      })
+
+      const { query } = createTestClient({ apolloServer })
+      const res: any = await query(loginQuery, {
+        variables: {
+          email: 'any_email@mail.com',
+          password: '123'
+        }
+      })
+      expect(res.data.login.accessToken).toBeTruthy()
+      expect(res.data.login.name).toBe('any_name')
+    })
+  })
+})
